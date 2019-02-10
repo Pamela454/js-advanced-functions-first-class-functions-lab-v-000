c32c8990e2d77f5f80b2f9667af5dd153c895cad
Pamela Frieze <pfrieze@gmail.com>
2019-02-10 22:35:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const returnLastTwoDrivers = function(drivers) {
   return drivers.slice(Math.max(drivers.length - 2, 1))
 };
 
-const selectingDrivers = returnFirstTwoDrivers.concat (["returnLastTwoDrivers"]);
+const selectingDrivers = returnFirstTwoDrivers.concat ([returnLastTwoDrivers]);
