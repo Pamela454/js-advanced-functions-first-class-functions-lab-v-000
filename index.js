23410326905f59f26d19dd948d4ed1b5bce62f0e
Pamela Frieze <pfrieze@gmail.com>
2019-02-10 22:38:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ const returnLastTwoDrivers = function(drivers) {
 };
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+  function(fare) {
+    fare *
+  }
+}
