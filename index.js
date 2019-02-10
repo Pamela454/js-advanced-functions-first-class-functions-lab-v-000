9e87cdacd17add637338d21f5db5cead351e95f6
Pamela Frieze <pfrieze@gmail.com>
2019-02-10 23:00:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-function fetchSpecifiedDrivers(drivers, returnFirstTwoDrivers) {
+function fetchSpecifiedDrivers(drivers, function) {
   
 }
 
